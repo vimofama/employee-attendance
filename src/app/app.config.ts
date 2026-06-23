f971486c1c156d5cd438a7d8a0682e735c29cc1f
Mateo <vimofama@outlook.com>
2026-06-23 11:59:55 -0500

    feat: integrate PrimeNG with Lara theme and update routing configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { roleGuard } from '@app/auth/guard/role-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.routes),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.routes').then((m) => m.routes),
+    // canActivate: [roleGuard],
+    data: {roles: ['admin']}
+  },
+  {
+    path: 'local',
+    loadChildren: () => import('./attendance/attendance.routes').then((m) => m.routes),
+    // canActivate: [roleGuard],
+    data: {roles: ['admin', 'local']}
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
